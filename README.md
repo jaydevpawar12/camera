125e08373ac66500d15ce8a1adb2962195c960a3
Jaydev <pawarjaydev96@gmail.com>
2023-02-21 21:42:18 +0530

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Authors
+
+- [@pawarjaydev](https://github.com/jaydevpawar12)
+
+
+A camera Progressive Web Application (PWA) is a web application that uses modern web technologies to deliver an app-like experience to users. It allows users to access a camera-based application from any web browser, including mobile devices. It utilizes a combination of HTML, CSS, JavaScript, and other web technologies to provide an app-like experience in a web browser. The application can use device-specific features such as access to camera, microphone, and other hardware and software features that are traditionally used in a native app. It also provides offline support, so users can access the application even when there is no internet connection. The application can also be installed on the device, making it available even when the device is offline.
+
+
---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+import {
+    startBtn,
+    captureBtn,
+    closeBtn,
+    player,
+    switchBtn,
+    canvas,
+    restartBtn,
+    mode,
+} from "./constants.js"
+
+import { closeCamera, openCamera } from "./camera.js"
+
+// let mode = "user"
+switchBtn.addEventListener("click", () => {
+    mode = mode === "environment" ? "user" : "environment"
+
+    player.classList.add("d-none")
+    const tracks = player.srcObject.getVideoTracks()
+    for (let i = 0; i < tracks.length; i++) {
+        tracks[i].stop()
+
+    }
+
+    openCamera()
+})
+
+startBtn.addEventListener("click", () => {
+    openCamera()
+})
+captureBtn.addEventListener("click", () => {
+    const preview = canvas.getContext("2d")
+    preview.drawImage(player, 0, 0, canvas.height, canvas.width)
+    canvas.classList.remove("d-none")
+    player.classList.add("d-none")
+    captureBtn.classList.add("d-none")
+    restartBtn.classList.remove("d-none")
+
+
+})
+closeBtn.addEventListener("click", () => {
+
+    closeCamera()
+})
+restartBtn.addEventListener("click", () => {
+    closeCamera()
+    openCamera()
+})
